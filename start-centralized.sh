5fde4cd08556c1986a88f3ee48f5db926ac9ce61
Victor's Github <constantinescu.victor01@gmail.com>
2026-01-19 21:53:33 +0200

    Revert "Merge branch 'main' of https://github.com/icordos/automotive-anomaly-detection"
    This reverts commit 1128383c4d4f5b4e3cdeb6246a1572a17aa737e5, reversing
    changes made to 238d658acec94e79fe0a1f221a9a77553fc77109.

--- start-centralized.sh
+++ start-centralized.sh
@@ -3,7 +3,7 @@
 python ./src/centralized/patchcore_training.py \
     --dataset-root data/raw \
     --output-dir artifacts/centralized \
-    --categories engine_wiring pipe_clip pipe_staple tank_screw underbody_pipes underbody_screw \
+    --categories engine_wiring pipe_clip \
     --image-size 512 --batch-size 4 --num-workers 4 \
     --coreset-method kcenter --coreset-ratio 0.1 --coreset-max-samples 5000 \
     --coreset-chunk-size 16384 --distance-chunk-size 8192 \
