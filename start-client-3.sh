ed911e0e584a01b7a9cfe3a69454de4927eb9aa0
Victor's Github <constantinescu.victor01@gmail.com>
2026-03-15 20:35:01 +0200

    fixed again

--- start-client-3.sh
+++ start-client-3.sh
@@ -21,5 +21,5 @@ python ./src/federated/client_sequential.py \
     --coreset-method kcenter --coreset-ratio 0.1 --coreset-max-samples 5000 \
     --coreset-chunk-size 16384 --distance-chunk-size 8192 \
     --interpretability --saliency-max-images 300 \
-    --interpretability True --shap-max-images 30\
+    --shap-max-images 30\
     --device cuda --log-level INFO
